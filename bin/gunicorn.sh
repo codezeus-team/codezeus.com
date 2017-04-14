70ddff1c19b87da7bcbbbbac696b1d8f52adaaf0
Jesse Boyer <JREAM@users.noreply.github.com>
2017-04-14 12:14:33 -0400

    Add a Gunicorn File to trigger

---
+++ bin/gunicorn.sh
@@ -0,0 +1,24 @@
+#!/bin/bash                                                                                          
+#
+# This Runs gunicorn from within the VirtualEnv
+# This is triggered by Upstart in /etc/init/gunicorn_apps.conf
+# ----------------
+# Creates log file in /var/log/gunicorn-apps.log
+
+# Dynamically calculate the suggested worker count.
+# The formula: 2n+1, eg: 2 cores=5, 3 cores=7, 4 cores=9
+worker_total=$(( 2 * `cat /proc/cpuinfo | grep 'core id' | wc -l` + 1 ))
+
+# Launch Gunicorn
+# Ensure its loaded from our virtualenvironment to use all the correct files,
+# hence the long path name.
+exec /home/user/deploy/.virtualenvs/codezeus.com/bin/gunicorn wsgi:application \
+  --name codezeus_com \
+  --workers $worker_total \
+  --log-level info \
+  --log-syslog \
+  --capture-output
+
+
+# To test the above command manually, remove "exec" from the line and copy/paste
+
